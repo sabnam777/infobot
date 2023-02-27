46e8c81d655ed546761d4ac8d08893c9f0299723
sabnam777 <115777974+sabnam777@users.noreply.github.com>
2023-02-27 15:26:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import os
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent, InlineQueryResultArticle
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 # Create bot instance
 bot = Client(
     "Donate",
-    bot_token=os.environ["5932409230:AAEDKc0qnKR57rNXNWvu6cxmqAzZCAklhx4"],
+    bot_token=os.environ["BOT_TOKEN"],
     api_id=int(os.environ["API_ID"]),
-    api_hash=os.environ["API_HASH"])
+    api_hash=os.environ["API_HASH"]
+)
 
 # Define home page with three buttons
 @bot.on_message(filters.command("start"))
@@ -20,38 +22,23 @@ def start(bot, update):
             InlineKeyboardButton("Join our group", url="t.me/your_group"),
             InlineKeyboardButton("Join our channel", url="t.me/your_channel")
         ],
-        [InlineKeyboardButton("Buy premium", url="t.me/your_premium_group")]
+        [InlineKeyboardButton("Premium", callback_data="premium")],
+        [InlineKeyboardButton("Donate", callback_data="donate")]
     ])
 
-    bot.send_message(
+    photo_url = "https://te.legra.ph/file/6750eaff7ac29676dbb31.jpg"
+    caption = f"Welcome {first_name}! Choose an option below:"
+    bot.send_photo(
         chat_id=chat_id,
-        text=f"Welcome {first_name}! Choose an option below:",
+        photo=photo_url,
+        caption=caption,
         reply_markup=markup
     )
 
-
-# Redirect to group and channel
-@bot.on_message(filters.regex(r"^Join our group$"))
-def group(bot, update):
-    chat_id = update.chat.id
-
-    bot.send_message(
-        chat_id=chat_id,
-        text="Join our group: t.me/your_group"
-    )
-
-@bot.on_message(filters.regex(r"^Join our channel$"))
-def channel(bot, update):
-    chat_id = update.chat.id
-
-    bot.send_message(
-        chat_id=chat_id,
-        text="Join our channel: t.me/your_channel"
-    )
-
-@bot.on_message(filters.regex(r"^Buy premium$"))
+# Premium page
+@bot.on_callback_query(filters.regex("premium"))
 def premium(bot, update):
-    chat_id = update.chat.id
+    chat_id = update.message.chat.id
 
     markup = InlineKeyboardMarkup([
         [InlineKeyboardButton("Join premium group", url="t.me/your_premium_group")]
@@ -66,9 +53,10 @@ def premium(bot, update):
         reply_markup=markup
     )
 
-@bot.on_message(filters.command("donate"))
+# Donation page
+@bot.on_callback_query(filters.regex("donate"))
 def donate(bot, update):
-    chat_id = update.chat.id
+    chat_id = update.message.chat.id
 
     markup = InlineKeyboardMarkup([
         [InlineKeyboardButton("Pay with Paytm", url="https://paytm.me/your_paytm_link")],
@@ -85,25 +73,14 @@ def donate(bot, update):
         reply_markup=markup
     )
 
-
-# Donation page
-@bot.message_handler(commands=['donate'])
-def donate(message):
-    markup = types.InlineKeyboardMarkup()
-    paytm_btn = types.InlineKeyboardButton(text='Pay with Paytm', url='https://paytm.me/your_paytm_link')
-    upi_btn = types.InlineKeyboardButton(text='Pay with UPI', url='upi://pay?pa=your_upi_id')
-    phonepay_btn = types.InlineKeyboardButton(text='Pay with PhonePe', url='https://www.phonepe.com/en/')
-    markup.add(paytm_btn, upi_btn, phonepay_btn)
-    photo_url = 'https://te.legra.ph/file/d9c29bfcfdaff38eb8a3a.jpg'
-    caption = 'Thank you for your donation!'
-    bot.send_photo(message.chat.id, photo_url, caption=caption, reply_markup=markup)
-
 # Broadcast feature for owner
-owner_id = "5143506371"
+owner_id = "1234567890"
 
 @bot.on_message(filters.command("broadcast") & filters.user(owner_id))
 def broadcast(bot, update):
+    # Add your broadcast code here
+    pass
 
 
 # Start the bot
-bot.polling()
+bot.run()
